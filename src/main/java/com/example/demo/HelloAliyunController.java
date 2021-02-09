6ed196904474b4aed96ab30482f415946153775c
王涛华 <73281045+wangtaohua@users.noreply.github.com>
2021-02-09 18:49:58 +0800

    亚当阿里云仓

---
+++ src/main/java/com/example/demo/HelloAliyunController.java
@@ -0,0 +1,1 @@
+package com.example.demo;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestControllerpublic class HelloAliyunController {    @RequestMapping("/")    public String helloAliyun(){        return "wthaizyc，亚当夏娃永远在一起!";    }}
